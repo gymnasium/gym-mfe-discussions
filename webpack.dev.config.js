ce9cbf4bcc31b308d06e5e81fcf20e9e68bd3810
Roman Edirisinghe <rediris@users.noreply.github.com>
2024-01-31 12:18:30 -0500

    Update webpack.dev.config.js

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,7 +3,7 @@ const { createConfig } = require('@edx/frontend-build');
 
 const config = createConfig('webpack-dev');
 
-config.cache = { type: 'filesystem' }; // This isn't needed but really speeds up rebuilds!
+// config.cache = { type: 'filesystem' }; // This isn't needed but really speeds up rebuilds!
 
 config.devServer.allowedHosts = [
   'apps.local.overhang.io',
@@ -13,12 +13,14 @@ config.experiments = {
   topLevelAwait: true
 };
 
+config.resolve.alias =  {
+  'gym-frontend-components': path.resolve(__dirname, '../gym-frontend-components/'),
+};
+
 config.resolve.modules = [
   path.resolve(__dirname, './node_modules'),
   path.resolve(__dirname, './src'),
   'node_modules',
 ];
-
-config.module.rules[0].exclude = /node_modules\/(?!(query-string|split-on-first|strict-uri-encode|@edx))/;
 
 module.exports = config;
