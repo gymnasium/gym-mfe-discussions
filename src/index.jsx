f5cefbca3664401229121fa2dc13f5a8ab3b78ee
Roman Edirisinghe <rediris@users.noreply.github.com>
2024-02-09 09:39:41 -0500

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,19 @@ import 'regenerator-runtime/runtime';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Helmet } from 'react-helmet';
 
 import {
-  APP_INIT_ERROR, APP_READY, initialize, mergeConfig,
+  APP_INIT_ERROR,
+  APP_READY,
+  getConfig,
+  initialize,
+  mergeConfig,
   subscribe,
 } from '@edx/frontend-platform';
 import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
+
+import GymSettings, { GymFooter, GymHeader } from '@edx/gym-frontend';
 
 import { DiscussionsHome } from './discussions';
 import messages from './i18n';
@@ -17,10 +24,26 @@ import store from './store';
 import './assets/favicon.ico';
 import './index.scss';
 
+const config = getConfig();
+const timestamp = Date.now();
+const settings = await GymSettings;
+const root = settings.urls.root; // should be same as marketing URL
+const css = `${root}${settings.css.mfe}?${timestamp}`;
+const title = `Discussions | ${getConfig().SITE_NAME}`;
+
 subscribe(APP_READY, () => {
   ReactDOM.render(
     <AppProvider store={store}>
+      <Helmet>
+        <title>{title}</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="shortcut icon" href={config.FAVICON_URL} type="image/x-icon" />
+        <link rel="stylesheet" href={css} />
+      </Helmet>
+      <GymHeader secondaryNav="courses" />
       <DiscussionsHome />
+      <GymFooter />
     </AppProvider>,
     document.getElementById('root'),
   );
